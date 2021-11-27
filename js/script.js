97594ca0004be241dea5a5645ff54798de0db85d
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2021-11-27 21:17:25 +0100

    write simple program

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+"use strict";
+//1
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+//2
+ const personalMovieDb = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+ };
+ const qest1 = prompt('Один из последних просмотренных фильмов?', '');
+ const qest2 = prompt('На сколько оцените его?', '');
+ const answer1 = prompt('Titanik','');
+ const answer2 = prompt('На сколько оцените его?','');
+ 
+ personalMovieDb.movies[qest1] = answer1;
+ personalMovieDb.movies[qest2] = answer2;
+ console.log(personalMovieDb);
 
 
 
